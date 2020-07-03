7ddd4d36ac45bf09a56cb1ea8fcde7d0d11fa01b
madhuvayya <madhu.vayya@gmail.com>
2020-07-03 09:14:48 +0530

    Added HelloWorldController class

---
+++ src/main/java/com/firstspringapp/HelloWorldController.java
@@ -0,0 +1,33 @@
+package com.firstspringapp;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @RequestMapping(value = {"", "/","/home"})
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name){
+        return "Hello "+ name;
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name){
+        return "Hello "+ name;
+    }
+
+    @PostMapping("/post")
+    public String sayHelloParam(@RequestBody User user){
+        return "Hello "+ user.getFirstName() +" "+ user.getLastName();
+    }
+
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName,  @RequestParam(value = "lastName") String lastName ){
+        return "Hello " + firstName +" "+ lastName;
+    }
+}
